6ace0900bbce53bac7109d03120bed08a9f5b823
Zeyad Mohammed Slama <zeyadslama23@gmail.com>
2024-08-17 23:02:44 +0300

    Adding DuckTyping

---
+++ DuckTyping.py
@@ -0,0 +1,28 @@
+
+
+class Animal:
+    alive = True
+    
+
+class Dog(Animal):
+    def speak(self) -> None:
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self) -> None:
+        print("MEWO!")
+
+
+class Car:
+    alive = False
+    
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()]
+
+
+# if it walks like a duck, and quacks like a duck, then it must be a duck.
+
+for animal in animals:
+    animal.speak()
