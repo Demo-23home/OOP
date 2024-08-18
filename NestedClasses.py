b2150466245f13f161f48359c3340046e5a1005b
demo-23home <zeyadslama23@gmail.com>
2024-08-18 14:31:37 +0300

    Adding StaticMethods

--- NestedClasses.py
+++ NestedClasses.py
@@ -1,32 +1,33 @@
 
 
 class Company:
-    def __init__(self, company_name):
+    def __init__(self, company_name:str) -> None:
         self.company_name = company_name
         self.employees = []
         
     class Employee:
-        def __init__(self, name, position):
+        def __init__(self, name:str, position:str) -> None:
             self.name = name
             self.position = position
             
-        def get_details(self):
+        def get_details(self) -> str:
             return f"{self.name} working as {self.position}"
     
         
-    def add_employee(self, name, position):
+    def add_employee(self, name:str, position:str) -> None:
         new_employee = self.Employee(name, position)
         self.employees.append(new_employee)
         
         
-    def list_employee(self):
+    def list_employee(self) -> list[str]:
         return [emp.get_details() for emp in self.employees]
     
 
 
 company = Company("NewCompany")
 company.add_employee("Zeyad", "SWE")
-company.add_employee("Zeyad", "SWE")
-company.add_employee("Zeyad", "SWE")
+company.add_employee("Mohammed", "Backend")
+company.add_employee("Salama", "Mobile")
 emps = company.list_employee()
-print(*emps)
+for emp in emps:
+    print(emp)
---
+++ StaticMethods.py
@@ -0,0 +1,31 @@
+class Employee:
+    def __init__(self, name:str, position:str) -> None:
+        self.name = name 
+        self.position = position
+        
+        
+    def get_details(self) -> str:
+        return f"{self.name} {self.position}"
+    
+    
+    @staticmethod
+    def is_valid_position(position:str) -> bool:
+        valid_positions = ["SWE", "Backend", "Mobile", "Frontend"]
+        return position in valid_positions
+    
+    
+emp1 = Employee("Zeyad", "Backend")    
+emp2 = Employee("Mohammed", "Frontend")    
+emp3 = Employee("Abdo", "Mobile")    
+  
+print(emp1.get_details()) 
+print(emp2.get_details()) 
+print(emp3.get_details()) 
+  
+    
+Employee.is_valid_position("SWE")
+
+
+Employee.is_valid_position("DevOps")
+    
+    
